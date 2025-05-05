e60c250c83be642c84c23b878ac4b8e6ef97e391
LokeshSaini <Stonehearted313@gmail.com>
2025-05-05 16:57:54 +0530

    Added to upload images and saved them on cloudinary

--- routes/listing.js
+++ routes/listing.js
@@ -1,24 +1,22 @@
 const express = require("express");
 const router = express.Router();
-const multer = require("multer");
-const upload = multer({ dest: "uploads/" });
 const wrapAsync = require("../utils/wrapAsync");
 const { validateListing, isLoggedIn, isOwner } = require("../middleware.js");
-
 const listingController = require("../controllers/listing.js");
+const multer = require("multer");
+const { storage } = require("../cloudConfig.js");
+const upload = multer({ storage });
 
 //Index Route
 router
   .route("/")
   .get(wrapAsync(listingController.index))
-  // .post(
-  //   isLoggedIn,
-  //   validateListing,
-  //   wrapAsync(listingController.createListing)
-  // );
-  .post(upload.single("listing[image]"), (req, res) => {
-    res.send(req.file);
-  });
+  .post(
+    isLoggedIn,
+    upload.single("listing[image]"),
+    validateListing,
+    wrapAsync(listingController.createListing)
+  );
 
 //New route
 router.get("/new", isLoggedIn, listingController.newListing);
@@ -38,6 +36,7 @@ router
   .put(
     isLoggedIn,
     isOwner,
+    upload.single("listing[image]"),
     validateListing,
     wrapAsync(listingController.updateListing)
   )
